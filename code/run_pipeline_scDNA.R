47d7683b89d24400a0e46ead9e3e601cf4f69721
XQBai <1349011195@qq.com>
2024-12-19 10:55:59 -0800

    update "run_pipeline_scDNA.R"

--- code/run_pipeline_scDNA.R
+++ code/run_pipeline_scDNA.R
@@ -1,8 +1,17 @@
 library(Seurat)
+library(optparse)
+source('/mnt/ix1/Projects/M070_200622_GI_multiomics/GithubCode_202411/code/Filter_noise_scDNA.R')
+source('/mnt/ix1/Projects/M070_200622_GI_multiomics/GithubCode_202411/code/Identify_cell_components_scDNA.R')
+source('/mnt/ix1/Projects/M070_200622_GI_multiomics/GithubCode_202411/code/Construct_subclone_scDNA.R')
+
+option_list = list(
+  make_option(c("-p", "--project_path"), action="store", default=NA, type='character',
+              help="Project name, default= sample name")
+)
+opt = parse_args(OptionParser(option_list=option_list))
 
 ## read the cell metrics file conducted by cellranger-dna
-per_cell_metrics <- read.csv(file.path(project_path, 'per_cell_summary_metrics.csv'), sep=",", header=T)
-project_path <- '../example/P5931/scDNA'
+per_cell_metrics <- read.csv(file.path(opt$project_path, 'per_cell_summary_metrics.csv'), sep=",", header=T)
 seurat_scDNA <- readRDS('seurat_scDNA.rds')
 scdna_matrix_locs <- readRDS('scdna_matrix_locs.rds')
 
